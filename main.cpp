10b3e84a59ad9746c1530f41a3fa96b598032f96
Sönke Lange <s.lange@mytaxi.com>
2016-01-22 01:31:35 +0100

    Add possibility to pause

--- main.cpp
+++ main.cpp
@@ -38,38 +38,47 @@ int main(int argc, char *argv[])
     TimeCalculator* timeCalculator = new TimeCalculator();
     TimeOutput* timeOutput = new TimeLogger(); // TODO: Add correct TimeOutput here
     
+    
+    bool paused = false;
     while (1) {
-        cv::Mat inputFrame;
-        videoCapture.read(inputFrame);
-        cv::Mat outputFrame;
-        inputFrame.copyTo(outputFrame);
-        
-        
-        // filter clock
-        filter->process(inputFrame);
-        cv::Point2d clockMiddle = filter->getClockCenterPosition();
-        cv::Point2d clockTop = filter->getClockTopPosition();
-        cv::Point2d bigHandTop = filter->getBigHandPosition();
-        cv::Point2d smallHandTop = filter->getSmallHandPosition();
-        
-        // calculate time
-        Time time = timeCalculator->calculcateTime(clockMiddle, clockTop, bigHandTop, smallHandTop);
-        
-        if (debug) {
-            cv::circle(outputFrame, clockMiddle, 100, cv::Scalar(255,0,0));
-            cv::circle(outputFrame, clockTop, 100, cv::Scalar(0,255,0));
-            cv::circle(outputFrame, bigHandTop, 100, cv::Scalar(0,0,255));
-            cv::circle(outputFrame, smallHandTop, 100, cv::Scalar(0,255,255));
+        if (!paused) {
+            cv::Mat inputFrame;
+            videoCapture.read(inputFrame);
+            cv::Mat outputFrame;
+            inputFrame.copyTo(outputFrame);
+            
+            
+            // filter clock
+            filter->process(inputFrame);
+            cv::Point2d clockMiddle = filter->getClockCenterPosition();
+            cv::Point2d clockTop = filter->getClockTopPosition();
+            cv::Point2d bigHandTop = filter->getBigHandPosition();
+            cv::Point2d smallHandTop = filter->getSmallHandPosition();
+            
+            // calculate time
+            Time time = timeCalculator->calculcateTime(clockMiddle, clockTop, bigHandTop, smallHandTop);
+            
+            if (debug) {
+                cv::circle(outputFrame, clockMiddle, 100, cv::Scalar(255,0,0));            // BLUE
+                cv::circle(outputFrame, clockTop, 100, cv::Scalar(0,255,0));         // GREEN
+                cv::circle(outputFrame, smallHandTop, 100, cv::Scalar(0,0,255));            // RED
+                cv::circle(outputFrame, bigHandTop, 100, cv::Scalar(0,255,255));        // YELLOW
+            }
+            
+            // output time
+            timeOutput->output(time);
+            
+            cv::imshow(sourceWindow, inputFrame);
+            cv::imshow(outputWindow, outputFrame);
         }
         
-        // output time
-        timeOutput->output(time);
-        
-        cv::imshow(sourceWindow, inputFrame);
-        cv::imshow(outputWindow, outputFrame);
-        
-        if(cv::waitKey(30) >= 0) {
+        int waitKey = cv::waitKey(30);
+        if(waitKey == -1) {
+            continue;
+        } else if (waitKey == 113) { // q
             break;
+        } else if (waitKey == 112) {
+            paused = !paused;
         }
     }
     delete filter;
