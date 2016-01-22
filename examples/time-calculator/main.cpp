2633d8a85cf8e811bb6f0ec3478894391228f7fb
Sönke Lange <s.lange@mytaxi.com>
2016-01-22 01:31:19 +0100

    Better TimeCalculator examples

--- examples/time-calculator/main.cpp
+++ examples/time-calculator/main.cpp
@@ -5,119 +5,43 @@
 int main(int argc, char *argv[])
 {
     TimeCalculator timeCalculator = TimeCalculator();
-    cv::Point2d clockMiddle = cv::Point2d(0, 0);
-    cv::Point2d clockTop = cv::Point2d(0, 6);
+
     
-    // 0.00
     std::cout << "========================================" << std::endl;
-    std::cout << "Expected: 0.00" << std::endl;
-    clockTop = cv::Point2d(0, 6);
-    cv::Point2d smallHandTop = cv::Point2d(0, 3);
-    cv::Point2d bigHandTop = cv::Point2d(0, 5);
-    Time time = timeCalculator.calculcateTime(clockMiddle, clockTop, bigHandTop, smallHandTop);
-    std::cout << "It's " << time.getHour() << ":" << time.getMinute() << "h!" << std::endl;
-    time.output();
+    std::cout << "Expected: 0.14" << std::endl;
+    cv::Point clockMiddle = cv::Point(166,190);
+    cv::Point clockTop = cv::Point(369,231);
+    cv::Point smallHand = cv::Point(282,208);
+    cv::Point bigHand = cv::Point(391,120);
+    Time time = timeCalculator.calculcateTime(clockMiddle, clockTop, bigHand, smallHand);
+    std::cout << "Actual: " << time.getHour() << ":" << time.getMinute() << std::endl;
     
-    // 0.15
     std::cout << "========================================" << std::endl;
-    std::cout << "Expected: 0.15" << std::endl;
-    clockTop = cv::Point2d(0, 6);
-    smallHandTop = cv::Point2d(0, 3);
-    bigHandTop = cv::Point2d(5, 0);
-    time = timeCalculator.calculcateTime(clockMiddle, clockTop, bigHandTop, smallHandTop);
-    std::cout << "It's " << time.getHour() << ":" << time.getMinute() << "h!" << std::endl;
-    time.output();
+    std::cout << "Expected: 0.14" << std::endl;
+    clockTop = cv::Point(367,442);
+    clockMiddle = cv::Point(368,225);
+    smallHand = cv::Point(370,305);
+    bigHand = cv::Point(215,222);
+    time = timeCalculator.calculcateTime(clockMiddle, clockTop, bigHand, smallHand);
+    std::cout << "Actual: " << time.getHour() << ":" << time.getMinute() << std::endl;
     
-    // 0.30
     std::cout << "========================================" << std::endl;
-    std::cout << "Expected: 0.30" << std::endl;
-    clockTop = cv::Point2d(0, 6);
-    smallHandTop = cv::Point2d(0, 3);
-    bigHandTop = cv::Point2d(0, -5);
-    time = timeCalculator.calculcateTime(clockMiddle, clockTop, bigHandTop, smallHandTop);
-    std::cout << "It's " << time.getHour() << ":" << time.getMinute() << "h!" << std::endl;
-    time.output();
+    std::cout << "Expected: 0.14" << std::endl;
+    clockTop = cv::Point(375,410);
+    clockMiddle = cv::Point(374,205);
+    smallHand = cv::Point(377,277);
+    bigHand = cv::Point(265,206);
+    time = timeCalculator.calculcateTime(clockMiddle, clockTop, bigHand, smallHand);
+    std::cout << "Actual: " << time.getHour() << ":" << time.getMinute() << std::endl;
     
-    // Between 0.35 and 0.40
     std::cout << "========================================" << std::endl;
-    std::cout << "Expected: Between 0.35 and 0.40" << std::endl;
-    clockTop = cv::Point2d(0, 6);
-    smallHandTop = cv::Point2d(0, 3);
-    bigHandTop = cv::Point2d(-4, -4);
-    time = timeCalculator.calculcateTime(clockMiddle, clockTop, bigHandTop, smallHandTop);
-    std::cout << "It's " << time.getHour() << ":" << time.getMinute() << "h!" << std::endl;
-    time.output();
-    
-    // Between 0.50 and 0.55
-    std::cout << "========================================" << std::endl;
-    std::cout << "Expected: Between 0.50 and 0.55" << std::endl;
-    clockTop = cv::Point2d(0, 6);
-    smallHandTop = cv::Point2d(0, 3);
-    bigHandTop = cv::Point2d(-4, 4);
-    time = timeCalculator.calculcateTime(clockMiddle, clockTop, bigHandTop, smallHandTop);
-    std::cout << "It's " << time.getHour() << ":" << time.getMinute() << "h!" << std::endl;
-    time.output();
-    
-    // 0.45
-    std::cout << "========================================" << std::endl;
-    std::cout << "Expected: 0.45" << std::endl;
-    clockTop = cv::Point2d(0,6);
-    smallHandTop = cv::Point2d(0, 3);
-    bigHandTop = cv::Point2d(-5, 0);
-    time = timeCalculator.calculcateTime(clockMiddle, clockTop, bigHandTop, smallHandTop);
-    std::cout << "It's " << time.getHour() << ":" << time.getMinute() << "h!" << std::endl;
-    time.output();
-    
-    // 3.15
-    std::cout << "========================================" << std::endl;
-    std::cout << "Expected: 3.15" << std::endl;
-    clockTop = cv::Point2d(-6,0);
-    smallHandTop = cv::Point2d(0, 3);
-    bigHandTop = cv::Point2d(0, 5);
-    time = timeCalculator.calculcateTime(clockMiddle, clockTop, bigHandTop, smallHandTop);
-    std::cout << "It's " << time.getHour() << ":" << time.getMinute() << "h!" << std::endl;
-    time.output();
-    
-    // 6.15
-    std::cout << "========================================" << std::endl;
-    std::cout << "Expected: 6.15" << std::endl;
-    clockTop = cv::Point2d(0,6);
-    smallHandTop = cv::Point2d(0, -3);
-    bigHandTop = cv::Point2d(5, 0);
-    time = timeCalculator.calculcateTime(clockMiddle, clockTop, bigHandTop, smallHandTop);
-    std::cout << "It's " << time.getHour() << ":" << time.getMinute() << "h!" << std::endl;
-    time.output();
-    
-    // 9.45
-    std::cout << "========================================" << std::endl;
-    std::cout << "Expected: 9.45" << std::endl;
-    clockTop = cv::Point2d(-6,0);
-    smallHandTop = cv::Point2d(0, -3);
-    bigHandTop = cv::Point2d(0, -5);
-    time = timeCalculator.calculcateTime(clockMiddle, clockTop, bigHandTop, smallHandTop);
-    std::cout << "It's " << time.getHour() << ":" << time.getMinute() << "h!" << std::endl;
-    time.output();
-    
-    
-    // Between 10.50 and 10.55
-    std::cout << "========================================" << std::endl;
-    std::cout << "Expected: Between 10.50 and 10.55" << std::endl;
-    clockTop = cv::Point2d(0, 6);
-    smallHandTop = cv::Point2d(-4, 4);
-    bigHandTop = cv::Point2d(-4, 4);
-    time = timeCalculator.calculcateTime(clockMiddle, clockTop, bigHandTop, smallHandTop);
-    std::cout << "It's " << time.getHour() << ":" << time.getMinute() << "h!" << std::endl;
-    time.output();
-    
-    // Between 9.45
-    std::cout << "========================================" << std::endl;
-    std::cout << "Expected: 9.45" << std::endl;
-    clockTop = cv::Point2d(0, -6);
-    smallHandTop = cv::Point2d(3, 0);
-    bigHandTop = cv::Point2d(5, 0);
-    time = timeCalculator.calculcateTime(clockMiddle, clockTop, bigHandTop, smallHandTop);
-    std::cout << "It's " << time.getHour() << ":" << time.getMinute() << "h!" << std::endl;
-    time.output();
+    std::cout << "Expected: 0.14" << std::endl;
+    clockTop = cv::Point(465,286);
+    clockMiddle = cv::Point(281,255);
+    smallHand = cv::Point(366,266);
+    bigHand = cv::Point(267,349);
+    time = timeCalculator.calculcateTime(clockMiddle, clockTop, bigHand, smallHand);
+    std::cout << "Actual: " << time.getHour() << ":" << time.getMinute() << std::endl;
     
     std::cin.get();
     
